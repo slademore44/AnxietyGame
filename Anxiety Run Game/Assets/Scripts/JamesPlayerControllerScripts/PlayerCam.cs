4f41b03d1ff97f0f428a948a9076b5ff95f9dba7
slademore44 <123640810+slademore44@users.noreply.github.com>
2023-12-01 13:47:04 +0000

    Organised Assets into folders and made player movement script

---
+++ Anxiety Run Game/Assets/Scripts/JamesPlayerControllerScripts/PlayerCam.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensY;
+    public float sensX;
+
+    public Transform orientation;
+
+    float yRotation;
+    float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation += mouseY;
+
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
