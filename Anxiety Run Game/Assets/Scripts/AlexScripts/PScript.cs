7b07156291d9ac006ca3fe5fdbbd64f1baaee220
AIexx9 <alexbury91@gmail.com>
2023-12-08 11:41:33 +0000

    Pendulum Script

---
+++ Anxiety Run Game/Assets/Scripts/AlexScripts/PScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PScript : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float limit = 75f;
+    public bool randomStart = false;
+    private float random = 0;
+
+    private void Awake()
+    {
+        if (randomStart)
+            random = Random.Range(0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = limit * Mathf.Sin(Time.time + random * speed);
+        transform.localRotation = Quaternion.Euler(angle, 0, 0);
+    }
+}
